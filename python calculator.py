a5619958e2e4337027693a1f4e16af26e05a013a
Zhanna <90461518+ZReplicant@users.noreply.github.com>
2021-10-01 08:15:43 +0300

    Correct mistakes

--- python calculator.py
+++ python calculator.py
@@ -1,18 +1,18 @@
-a = int(input())
-oper = str(input())
-b = int(input())
+a = int(input('Type the first number'))
+oper = input('Choose the operation\n+, -, *, / or :, //, %')
+b = int(input('Type the second number'))
 if b == 0 and (oper == '/' or oper == ':' or oper == '//' or oper == '%'):
     print('I hate zero division, type something else')
 elif oper == '+':
-    print(a + b)
+    res = a + b
 elif oper == '-':
-    print(a - b)
+    res = a - b
 elif oper == '*':
-    print(a * b)
-elif oper == '/' | oper == ':':
-    print(a / b)
+    res = a * b
+elif oper == '/' or oper == ':':
+    res = a / b
 elif oper == '//':
-    print(a // b)
+    res = a // b
 elif oper == '%':
-    print(a % b)
-
+    res = a % b
+print(res)
